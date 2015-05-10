b6ffccef1467694fca12e3be7326acc3e1820acb
Marton Bodonyi <marty@interactivejunky.com>
2015-05-10 18:29:08 +1000

    Update grunt file to include angular2

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,33 @@ module.exports = function(grunt){
         command: ['bower install',
           'rm -r libraries/*',
           'mv bower_components/** libraries/',
-          'rm -rf bower_components'
+          'rm -rf bower_components',
+          'rm -rf dist/js/lib && mkdir dist/js/lib',
+          'mv libraries/angular2/index.js dist/js/lib/angular2.js',
+          'tsd query angular2 --action install',
+          'mv typings/angular2 src/ts/angular2',
+          'rm -rf typings'
         ].join('&&')
+      }
+    },
+    typescript: {
+      base: {
+        src: ['src/ts/**/*.ts'],
+        dest: 'docroot/js/app.js',
+        options: {
+          module: 'amd', //or commonjs
+          target: 'es5', //or es3
+          basePath: 'docroot/ts',
+          sourceMap: true,
+          declaration: true,
+          watch: true
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-typescript');
 
 //Default Tasks
   grunt.registerTask('default',['shell']);
