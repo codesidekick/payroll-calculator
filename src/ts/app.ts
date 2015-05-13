f4207161ebf7ab11f64fb7631f240ab55464f1b5
Marton Bodonyi <marty@interactivejunky.com>
2015-05-12 20:49:27 -0700

    Create GitHub pages branch

--- src/ts/app.ts
+++
@@ -1,8 +0,0 @@
-// Define the Angular module for our application.
-var app = angular.module("app", ["ui.bootstrap"]);
-
-app.factory('taxAustralia', function () {
-    return new SimplePayslip.TaxAustralia();
-});
-
-app.controller("PayrollController", ["$scope", "taxAustralia", SimplePayslip.PayrollController]);
